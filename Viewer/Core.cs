27c1cce50ad753c5bd17a2d5a4b1fdf27e57fa42
Yin.Luo <Yin.Luo@kuka.com>
2023-02-09 16:57:08 +0800

    Add project files.

---
+++ Viewer/Core.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace View
+{
+    public static class Core
+    {
+        public static readonly string picname = @"output.bmp";
+
+        public static void Run()
+        {
+            int nx = 200;
+            int ny = 100;
+
+            using (Image<Rgba32> image = new(nx, ny))
+            {
+                for (int y = 0; y < ny; y++)
+                {
+                    for (int x = 0; x < nx; x++)
+                    {
+                        byte r = (byte)(255.99 * (float)x / (float)nx);
+
+                        byte g = (byte)(255.99 * (float)(ny - y - 1) / (float)ny);
+
+                        image[x, y] = new Rgba32(r, g, 50);
+                    }
+                }
+
+                image.SaveAsBmp(picname);
+            }
+        }
+    }
+}
---
+++ Viewer/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Core.Run();
+            InitializeComponent();
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(Path.Combine(Environment.CurrentDirectory, Core.picname));
+            bitmap.EndInit();
+
+            image.Source = bitmap;
+        }
+    }
+}
