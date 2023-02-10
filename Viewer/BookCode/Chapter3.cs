97d396fe631076b0b7bf047ab7980c05909842b2
Yin.Luo <Yin.Luo@kuka.com>
2023-02-10 11:41:20 +0800

    feat: Add Chapter3 code

--- Viewer/BookCode/Chapter3.cs
+++ Viewer/BookCode/Chapter3.cs
@@ -1,5 +1,6 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using System.Numerics;
 
 namespace Viewer.BookCode
 {
@@ -7,7 +8,44 @@ namespace Viewer.BookCode
     {
         public static Image<Rgba32> GenerateImage()
         {
-            return null;
+            int nx = 400;
+            int ny = 200;
+            Image<Rgba32> image = new(nx, ny);
+
+            Vector3 origin = Vector3.Zero;
+
+            float width = 4;
+            float height = 2;
+            float dist = 1;    // 画面距相机的距离
+
+            float widthStep = width / (nx - 1);
+            float heightStep = height / (ny - 1);
+
+            Vector3 bottom_left_corner = new Vector3(-width / 2, -height / 2, -dist);
+
+            // 从下往上一行一行扫描
+            for (int y = ny - 1; y >= 0; y--)
+            {
+                int dy = ny - 1 - y;
+
+                for (int x = 0; x < nx; x++)
+                {
+                    Vector3 curPt = bottom_left_corner + new Vector3(x * widthStep, dy * heightStep, 0);
+
+                    Ray ray = new Ray(origin, curPt);   // actually should be "new Ray(origin , curPt - origin)
+
+                    image[x, y] = new Rgba32(GetColor(ref ray));
+                }
+            }
+
+            return image;
+        }
+
+        private static Vector3 GetColor(ref Ray ray)
+        {
+            var dir = ray.Direction;
+            float t = 0.5f * (dir.Y + 1.0f);
+            return (1.0f - t) * Vector3.One + t * new Vector3(0.5f, 0.7f, 1.0f);
         }
     }
 }
--- Viewer/Core.cs
+++ Viewer/Core.cs
@@ -8,7 +8,7 @@ namespace Viewer
     {
         public static Image<Rgba32> GenerateImage()
         {
-            return Chapter1.GenerateImage();
+            return Chapter3.GenerateImage();
         }
     }
 }
